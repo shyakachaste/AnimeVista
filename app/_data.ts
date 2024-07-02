6ef6eebb1e4c36d6180195124315089d17b8744e
Shyaka Chaste <liochastej@gmail.com>
2024-07-02 13:36:55 -0700

    starting

---
+++ app/_data.ts
@@ -0,0 +1,140 @@
+export const data = [
+  {
+    id: "1",
+    name: "Kuroko",
+    image: {
+      original:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt2348803%2F&psig=AOvVaw2-qHai55_kqJAaoHTEoUgJ&ust=1720038662923000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJjYj7-ZiYcDFQAAAAAdAAAAABAJ",
+    },
+    kind: "TV",
+    episodes: 366,
+    episodes_aired: 366,
+    score: "7.92",
+  },
+  {
+    id: "2",
+    name: "kaiju_No8",
+    image: {
+      original: "",
+    },
+    kind: "TV",
+    episodes: 170,
+    episodes_aired: 170,
+    score: "7.16",
+  },
+  {
+    id: "3",
+    name: "dragon_ball",
+    image: {
+      original:
+        "https://m.media-amazon.com/images/M/MV5BMGMyOThiMGUtYmFmZi00YWM0LWJiM2QtZGMwM2Q2ODE4MzhhXkEyXkFqcGdeQXVyMjc2Nzg5OTQ@._V1_FMjpg_UX1000_.jpg",
+    },
+    kind: "TV",
+    episodes: 153,
+    episodes_aired: 153,
+    score: "8.68",
+  },
+  {
+    id: "4",
+    name: "jujutsu_kaisen",
+    image: {
+      original:
+        "https://static.wikia.nocookie.net/jujutsu-kaisen/images/8/88/Anime_Key_Visual_2.png/revision/latest?cb=20201212034001",
+    },
+    kind: "TV",
+    episodes: 24,
+    episodes_aired: 24,
+    score: "8.78",
+  },
+  {
+    id: "5",
+    name: "death_Note",
+    image: {
+      original: "",
+    },
+    kind: "TV",
+    episodes: 64,
+    episodes_aired: 64,
+    score: "9.24",
+  },
+  {
+    id: "6",
+    name: "naruto",
+    image: {
+      original:
+        "https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg",
+    },
+    kind: "TV",
+    episodes: 220,
+    episodes_aired: 220,
+    score: "8.3",
+  },
+  {
+    id: "7",
+    name: "wind_breaker",
+    image: {
+      original: "",
+    },
+    kind: "TV",
+    episodes: 367,
+    episodes_aired: 367,
+    score: "9.0",
+  },
+  {
+    id: "9",
+    name: "one_piece",
+    image: {
+      original: "",
+    },
+    kind: "TV",
+    episodes: 1030,
+    episodes_aired: 1030,
+    score: "8.58",
+  },
+  {
+    id: "10",
+    name: "demon_slayer",
+    image: {
+      original:
+        "https://m.media-amazon.com/images/M/MV5BZjZjNzI5MDctY2Y4YS00NmM4LTljMmItZTFkOTExNGI3ODRhXkEyXkFqcGdeQXVyNjc3MjQzNTI@._V1_.jpg",
+    },
+    kind: "TV",
+    episodes: 26,
+    episodes_aired: 26,
+    score: "8.6",
+  },
+  {
+    id: "11",
+    name: "attack_on_titan",
+    image: {
+      original: "https://flxt.tmsimg.com/assets/p10701949_b_v8_ah.jpg",
+    },
+    kind: "TV",
+    episodes: 75,
+    episodes_aired: 75,
+    score: "9.16",
+  },
+  {
+    id: "12",
+    name: "hunter_x_hunter",
+    image: {
+      original:
+        "https://m.media-amazon.com/images/M/MV5BZjNmZDhkN2QtNDYyZC00YzJmLTg0ODUtN2FjNjhhMzE3ZmUxXkEyXkFqcGdeQXVyNjc2NjA5MTU@._V1_FMjpg_UX1000_.jpg",
+    },
+    kind: "TV",
+    episodes: 148,
+    episodes_aired: 148,
+    score: "9.1",
+  },
+  {
+    id: "13",
+    name: "bleach",
+    image: {
+      original: "",
+    },
+    kind: "TV",
+    episodes: 114,
+    episodes_aired: 114,
+    score: "8.39",
+  },
+];
